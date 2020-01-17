5df146071c48c29eb5dca5c3ff875c62a39343bd
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-17 23:22:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,8 @@ class Artist
   
   attr_accessor :name
   
+  @@song_count = 0
+  
   def initialize(name)
     @name = name
     @songs = []
@@ -10,9 +12,15 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
   
   def songs
     @songs    
   end
+  
+  def self.song_count
+    @@song_count
+  end
+  
 end
