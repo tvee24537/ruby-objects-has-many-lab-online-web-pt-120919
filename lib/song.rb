3b2bb354e03217a72914490971a103780f4ccbc7
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-17 23:24:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,24 @@
 class Song
 
-  attr_accessor :title, :artist
+  attr_accessor :name, :artist
+  
+  @@all = []
 
+  def initialize(name, artist)
+    @name = name
+    @artist = artist
+    @@all << self
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def self.all
+    @@all
+  end
 end
