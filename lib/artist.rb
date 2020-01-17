0d847b6c8902be3e88fd34e173046186cd34ba48
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-17 23:29:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,14 +10,14 @@ class Artist
   end
   
   def add_song(song)
-    @songs << song
+    self.songs << song
     song.artist = self
     @@song_count += 1
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
+    self.songs << song
     song.artist = self
     @@song_count += 1
   end
