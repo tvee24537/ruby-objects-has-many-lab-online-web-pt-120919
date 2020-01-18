2ec2137d62da31085b034e9642bcd1b370372140
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-18 00:54:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,31 @@
 class Artist
-  
-  attr_accessor :name, :songs
-  
-  @@song_count = 0
-  
+  attr_accessor :name
+
+  @@all = []
+
   def initialize(name)
     @name = name
-    @songs = []
+    @@all << self
   end
-  
+
+  def self.all
+    @@all
+  end
+
   def add_song(song)
-    @songs << song
     song.artist = self
-    @@song_count += 1
   end
-  
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    @songs << song
+
+  def add_song_by_name(name)
+    song = Song.new(name)
     song.artist = self
-    @@song_count += 1
   end
-  
-  def self.song_count
-    @@song_count
-  end
-  
+
   def songs
-    @songs
+    Song.all.select {|song| song.artist == self}
   end
-  
+
+  def self.song_count
+    Song.all.count
+  end
 end
